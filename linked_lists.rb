50fb697d0385298d5c00b91a50b4bb34a66343af
Javi <jav.olivares3@gmail.com>
2022-03-25 16:31:36 -0500

    added pop,contains?,find and to_s methods

--- linked_lists.rb
+++ linked_lists.rb
@@ -52,6 +52,47 @@ class LinkedList
     end
     pointer
   end
+
+  def pop
+    pop_tail = self.tail
+    pointer = self.head
+    until pointer.next_node == self.tail
+      pointer = pointer.next_node
+    end
+    pointer.next_node = nil
+    self.tail = pointer
+    pop_tail
+  end
+
+  def contains?(value)
+    pointer = self.head
+    until pointer.nil? 
+      return true if pointer.value == value
+      pointer = pointer.next_node
+    end
+    false
+  end
+
+  def find(value)
+    i = 0
+    pointer = self.head
+    until pointer.nil?
+      return i if pointer.value == value
+      i += 1
+      pointer = pointer.next_node 
+    end
+    nil
+  end
+
+  def to_s
+    string = ""
+    pointer = self.head
+    until pointer.nil?
+      string << "(" << pointer.value.to_s << ") -> "
+      pointer = pointer.next_node
+    end
+    string << "nil"
+  end
 end
 
 class Node
@@ -74,6 +115,16 @@ list.prepend("IM the head")
 p list
 
 puts "size:#{list.size}"
-puts list.head
-puts list.tail
-puts list.at(2)
+#puts list.head
+#puts list.tail
+#puts list.at(2)
+puts "pop: #{list.pop}"
+p list
+puts list.size
+puts list.contains?(777)
+puts list.contains?(69)
+puts list.contains?(42)
+
+puts list.find(69)
+puts list.find(420)
+puts list.to_s
